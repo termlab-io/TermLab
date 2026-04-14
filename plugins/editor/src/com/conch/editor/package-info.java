ddb2cc046e3b547275046083364f0c251fcf88a7
an0nn30 <an0nn30@protonmail.com>
2026-04-14 13:54:53 -0500

    feat(editor): scaffold empty com.conch.editor plugin module

---
+++ plugins/editor/src/com/conch/editor/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Opt-in light editor plugin for Conch Workbench. Provides scratch
+ * file creation and SFTP-triggered local/remote file editing. See
+ * docs/superpowers/specs/2026-04-14-light-scratch-editor-design.md.
+ */
+package com.conch.editor;
